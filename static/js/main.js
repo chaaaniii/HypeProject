0d1bd1a874e5a114aefb5728ab23d09804a60fad
Gon <hss3522@gmail.com>
2022-11-21 11:33:19 +0900

    Let's start

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+import { handleLocation , route } from "./router.js";
+
+// hash url 변경 시 처리
+window.addEventListener("hashchange", handleLocation);
+
+// 첫 랜딩 또는 새로고침 시 처리
+document.addEventListener("DOMContentLoaded", handleLocation);
+
+// 전역 함수 리스트
+window.route = route;
