1e7d8002e925752bf1bd6e4e5281edcdfba7f1bb
jinyang <dbsskdud60@gmail.com>
2022-11-23 16:22:13 +0900

    로그인 회원가입 파이어베이스 완료

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 import { authService } from "./firebase.js";
 import { handleLocation, route } from "./router.js";
-import { socialLogin } from "./auth.js";
-import { handleAuth } from "./auth.js";
+import { logout } from "./auth.js";
+import { socialLogin, handleAuth } from "./auth.js";
 
 // hash url 변경 시 처리
 window.addEventListener("hashchange", handleLocation);
@@ -19,14 +19,22 @@ document.addEventListener("DOMContentLoaded", () => {
       Mp.style.visibility = "visible";
       let hidden = document.querySelector("#sign_in");
       hidden.style.display = "none";
+
+      document.querySelector("#login_out").innerText = "Log out";
     } else {
       // 로그아웃 상태인 경우
     }
   });
 });
 
+document.querySelector("#login_out").addEventListener("click", () => {
+  if (document.querySelector("#login_out").innerText === "Log out") {
+    logout();
+  }
+});
+
 // 전역 함수 리스트
 window.route = route;
 window.handleAuth = handleAuth;
 window.socialLogin = socialLogin;
-// window.logout = logout; 아직 미지정 오류나서 주석처리함
+window.logout = logout;
