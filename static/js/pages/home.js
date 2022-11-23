d39c0188c5b35306116f9711772dc3df88060bd0
chaaaniii <104333938+chan123456789@users.noreply.github.com>
2022-11-24 08:51:57 +0900

    1124_morning

--- static/js/pages/home.js
+++ static/js/pages/home.js
@@ -39,14 +39,12 @@ const nav_menu = document.querySelector(".nav_menu");
 const navBar = document.querySelector(".navBar");
 
 const nav_visi = () => {
-  console.log("a")
   let path = window.location.hash.replace("#", "");
 if(path === '' || path !=='login'){
   window.location.href='#login';
   nav_menu.style.visibility = "hidden";
   navBar.style.backgroundColor='white';
 }else{
-  console.log('aa')
   nav_menu.style.visibility = "visible";
   navBar.style.backgroundColor="black";
 }
@@ -54,7 +52,9 @@ if(path === '' || path !=='login'){
 
 const nav_visi_sign = () => {
   let path = window.location.hash.replace("#", "");
-if(path === 'signin'){
+  debugger;
+if(path === '' || path !=='signin'){
+  window.location.href='#signin';
   nav_menu.style.visibility = "hidden";
   navBar.style.backgroundColor='white';
 }else{
