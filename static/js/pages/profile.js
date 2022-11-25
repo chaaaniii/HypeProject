f08ff7d16fd1d62926b33e401daad235049152bd
Gon <hss3522@gmail.com>
2022-11-25 17:08:42 +0900

    mypost

--- static/js/pages/profile.js
+++ static/js/pages/profile.js
@@ -55,7 +55,7 @@ export const onFileChange = (event) => {
   reader.readAsDataURL(theFile); // file 객체를 브라우저가 읽을 수 있는 data URL로 읽음.
   reader.onloadend = (finishedEvent) => {
     // 파일리더가 파일객체를 data URL로 변환 작업을 끝났을 때
-    const imgDataUrl = finishedEvent.currentTarget.result;HypeList
+    const imgDataUrl = finishedEvent.currentTarget.result;
     localStorage.setItem("imgDataUrl", imgDataUrl);
     document.getElementById("profileView").src = imgDataUrl;
   };
